b4c323a033747894178ddd2db2e6c9a5cf04e65b
JR <jrfrazier21@gmail.com>
2018-01-07 21:19:20 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+@@all = []
+attr_accessor :name
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def new(name)
+  @@all << name
+end
+
+def self.all
+  @@all.each {|x| puts x.name}
+end
+
+def self.clear_all
+  @@all.clear
+end
+end
