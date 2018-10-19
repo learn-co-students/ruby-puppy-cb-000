883403e81a6cf4ae6f154738d7443533d704d2ef
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-19 21:24:13 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  @@all = []
+  attr_accessor :name
+
+  def self.clear_all()
+    @@all = []
+  end
+
+  def self.all()
+    @@all.each{|dog| puts dog.name}
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+end
